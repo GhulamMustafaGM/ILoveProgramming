bc7bfabce5b8197060a65b616961337850bc5de4
Ghulam Mustafa <mustafaji@gmail.com>
2019-10-22 23:32:10 +0200

    Added UserInput Number

---
+++ UserInput.cpp
@@ -0,0 +1,17 @@
+// User Input Code
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int AnyNumber;
+
+	cout << "Enter the AnyNumber: ";
+	cin >> AnyNumber;
+
+	cout << "You entered number. " << AnyNumber;
+	cout << endl;
+	
+	return 0;
+}
