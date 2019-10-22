31aca0174882c6379ccb2036074699e50a3099a1
Ghulam Mustafa <mustafaji@gmail.com>
2019-10-22 19:27:46 +0200

    Print characters & ACCI values

---
+++ PrintChACCI.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	char chr;
+	int value;
+
+	for (value = 1; value < 255; value++)
+	{
+		cout << value << "->" << chr << "\t";
+
+	}
+    cout << endl;
+	return 0;
+}
+
