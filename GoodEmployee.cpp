3da15761322182359a6d6cb07d575a106158730d
Ghulam Mustafa <mustafaji@gmail.com>
2019-10-22 23:19:12 +0200

    Commiting a good employee

---
+++ GoodEmployee.cpp
@@ -0,0 +1,24 @@
+// Good Employee
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int i = 10;
+	int j;
+
+	cout << "Enter 10.";
+	cin >> j;
+
+	if (j == i)
+	{
+		cout << "What a good Employee!!" << endl; 
+	}
+	else
+	{
+		cout << "Oops..., your are not selected at the movment." << endl;
+	}
+
+	return 0;
+}
