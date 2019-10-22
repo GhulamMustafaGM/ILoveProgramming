b6e87ba81cb17489c4de7e32d9800f1459da2a02
Ghulam Mustafa <mustafaji@gmail.com>
2019-10-22 19:42:56 +0200

    Comiting done

---
+++ PrintName.cpp
@@ -0,0 +1,15 @@
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	char printstr[20];
+	cout << "Enter your good name:";
+	cin >> printstr;
+	cout << "Hi, " << printstr << "! You are doing practice. " << endl;
+
+	return 0;
+}
+
+
