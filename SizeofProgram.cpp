ea4d710f802e8b9b2733e9e59a4a60bf8caa997f
Ghulam Mustafa <mustafaji@gmail.com>
2019-10-25 15:35:48 +0200

    Sizeof Integers values

---
+++ SizeofProgram.cpp
@@ -0,0 +1,16 @@
+// Sizeof Integers program
+
+#include<iostream>
+using namespace std;
+
+int main()
+
+{
+    cout << "Size of Char is " << sizeof(char) << " byte." << endl;
+    cout << "Size of short int is " << sizeof(short int) << " bytes." << endl;
+    cout << "size of int is " << sizeof(int) << " bytes." << endl;
+    cout << "size of long int is " << sizeof(long int) << " bytes." << endl;
+    cout << "size of long double is " << sizeof(long double) << " bytes" << endl;
+
+    return 0;
+}
