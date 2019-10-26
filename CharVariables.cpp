73f40f532f1b299af1fef3661b85e8e8959eae35
Ghulam Mustafa <mustafaji@gmail.com>
2019-10-26 14:08:02 +0200

    CharVariables

---
+++ CharVariables.cpp
@@ -0,0 +1,23 @@
+// Char veriables program
+
+#include<iostream>
+using namespace std;
+
+int main()
+
+{
+    char firstCh ='A';  // Declaration and initialization
+    char secondCh = 65;
+    char thirdCh = 'B';
+    char fourthCh = 66;
+    char fifthCh = 'C';
+
+
+    cout << "Value of firstCh: " << firstCh << endl;
+    cout << "Value of secondCh: " << secondCh<< endl;
+    cout << "Value of thirdCh: " << thirdCh << endl;
+    cout << "Value of fourthCh: " << fourthCh << endl;
+    cout << "Value of fifthCh: " << fifthCh<< endl;
+
+    return 0;
+}
