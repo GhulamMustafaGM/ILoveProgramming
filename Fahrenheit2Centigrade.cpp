49340bc2168766c964a0de4300efc3616cb41fdc
Ghulam Mustafa <mustafaji@gmail.com>
2019-10-27 14:40:34 +0100

    Fahrenheit and Centigrade Counting

--- Fahrenheit2Centigrade.cpp
+++ Fahrenheit2Centigrade.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 int main()
 {
-	float fahrenheit, celsius; // declared variables
+	float fahrenheit, celsius, ; // declared variables
 
 	cout << "\nEnter temperature in Fahrenheit:";
 	cin >> fahrenheit;
@@ -12,6 +12,6 @@ int main()
 	celsius = (fahrenheit - 32) / 1.8;
 	cout << "Temperature in Celsius = " << celsius;
 	cout << endl;
-	
+
 	return 0;
 }
