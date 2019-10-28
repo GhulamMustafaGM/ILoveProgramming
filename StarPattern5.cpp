7fc79e2237eb00d8c9c8c790d72c5f280847ec41
Ghulam Mustafa <mustafaji@gmail.com>
2019-10-28 14:46:40 +0100

    StarPattern5

---
+++ StarPattern5.cpp
@@ -0,0 +1,21 @@
+// StartPattern5
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c = 1;
+
+    for( a = 0; a < 5; a++)
+    {
+        for( b = 0; b <= a; b++)
+        {
+            cout << c << " ";
+            c++;
+        }
+        
+        cout << endl;
+    }
+    return 0;
+}
