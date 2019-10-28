f3823150d2c1f48479be1bda94a7ab931fc0180f
Ghulam Mustafa <mustafaji@gmail.com>
2019-10-28 14:39:54 +0100

    StartPattern4

---
+++ StarPattern4.cpp
@@ -0,0 +1,27 @@
+// StartPattern3
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c = 17, time = 1;
+
+    for( a = 0; a < 5; a++)
+    {
+        for( b = 0; b <= c; b++)
+        {
+            cout << " ";
+        }
+        c = c - 4;
+        for ( b = 0; b <= time; b++)
+        {
+            cout << "* ";
+        }
+
+        time = time + 2; 
+        
+        cout << endl;
+    }
+    return 0;
+}
