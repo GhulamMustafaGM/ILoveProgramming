967c61112afe8073ce79ed993313993e204248b8
Ghulam Mustafa <mustafaji@gmail.com>
2019-10-28 15:52:33 +0100

    StarPattern15

---
+++ StarPattern15.cpp
@@ -0,0 +1,33 @@
+// StarPattern15
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+	int i, j, k, decr=8, count=64, temp;
+	char ch='A';
+
+	for(i=0; i<5; i++)
+	{
+		for(k=0; k<decr; k++)
+		{
+			cout<<" ";
+		}
+		for(j=0; j<i+1; j++)
+		{
+			count++;
+		}
+		ch = count;
+		temp = ch;
+		for(j=0; j<i+1; j++)
+		{
+			cout<<ch--<<" ";
+		}
+		cout<<"\n";
+		ch = temp;
+		decr=decr-2;
+	}
+	return 0;
+}
