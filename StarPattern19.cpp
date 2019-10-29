baf09de2665d884b808a408471f8c2cc538ae1b3
Ghulam Mustafa <mustafaji@gmail.com>
2019-10-29 18:08:15 +0100

    StarPattern19

---
+++ StarPattern19.cpp
@@ -0,0 +1,21 @@
+// StarPattern19 - Similing face 
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int smile = 1, a, limit;
+	char ch = smile;
+	
+	cout << "Enter number of smiley face you want to print ? ";
+	cin >> limit;
+
+	for (a = 0; a < limit; a++)
+	{
+		cout << ch << " ";
+	}
+    cout << endl;
+    
+	return 0;
+}
