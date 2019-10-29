d665bc24cc59b6d03a14bcce2c2ea023a7636d09
Ghulam Mustafa <mustafaji@gmail.com>
2019-10-29 20:33:18 +0100

    UsefulSpecial characters

---
+++ UsefulSpecialChars.cpp
@@ -0,0 +1,18 @@
+// Useful special characters
+
+#include <iostream>
+using namespace std;
+
+int main ()
+    {
+    cout << "Hej Hej \n";
+    cout << "Hello\t friends." << endl;
+    cout << "Marhaba \ Marhaba." << endl; 
+    cout << "Hello\r Sir." << endl;
+    cout << "Might is right!\'." << endl;
+    cout << "Practice make man perfect.\"." <<endl;
+    cout << "One on one meeting \\.";
+    cout << endl;
+    
+    return 0;
+}
