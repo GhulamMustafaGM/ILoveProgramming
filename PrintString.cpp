0e9539464ae3faa5f808b742e76e87ed6353d37c
Ghulam Mustafa <mustafaji@gmail.com>
2019-10-30 18:59:06 +0100

    PrintString

---
+++ PrintString.cpp
@@ -0,0 +1,18 @@
+// Print String
+
+#include<iostream>
+#include<string>
+#include<cstdio>
+
+using namespace std;
+int main()
+{
+	char strs[25];
+	cout<<"Enter your first name : ";
+	cin >> strs;
+    
+	cout<<"Hi, "<<strs;
+	cout << endl;
+
+    return 0;
+}
