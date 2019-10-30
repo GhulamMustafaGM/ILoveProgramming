459aae0bd39643994af09a2bda6fbd50c500672b
Ghulam Mustafa <mustafaji@gmail.com>
2019-10-30 19:27:12 +0100

    CopyString

---
+++ CopyString.cpp
@@ -0,0 +1,24 @@
+// Print: Copy String
+
+#include<iostream>
+#include<cstring>
+#include<cstdio>
+
+using namespace std;
+
+int main()
+{
+	char strs1[20], strs2[20];
+
+	cout<<"Enter a string : ";
+	cin >>strs1;
+
+	cout<<"copying string 1 into string 2 ... \n";
+	
+	(strcpy(strs2, strs1);
+
+	cout<<"String 2 after copying "<<strs2;
+    cout << endl;
+	
+    return 0;
+}
