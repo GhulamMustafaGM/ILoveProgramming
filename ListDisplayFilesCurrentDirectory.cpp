89a51259b9941debb51df38e2e497cd29d861ace
Ghulam Mustafa <mustafaji@gmail.com>
2019-10-31 18:04:31 +0100

    ListDisplayfilesCurrentDirectory

--- ListDisplayFilesCurrentDirectory.cpp
+++ ListDisplayFilesCurrentDirectory.cpp
@@ -8,16 +8,17 @@ using namespace std;
 int main()
 {
     DIR *dir;
-	dirent *pdir;
-	cout<<"Press any key to list and view all the files in the current directory : \n";
-	dir=opendir(".");
+    dirent *pdir;
+	
+    cout<<"Press any key to list and view all the files in the current directory : \n";
+	getch();
+	
+    dir=opendir(".");
 	while(readdir(dir))
 	{
 		pdir=readdir(dir);
 		cout<<pdir->d_name<<"\t";
 	}
 	closedir(dir);
-
-
     cout << endl;
 }
