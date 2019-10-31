52fc28a2927ce9d1578ea0007bee6a905fe64e80
Ghulam Mustafa <mustafaji@gmail.com>
2019-10-31 17:10:24 +0100

    GenerateDifferentRandomNumbers;

---
+++ GenerateDiffRandomNum.cpp
@@ -0,0 +1,20 @@
+// Generate Diferent Random numbers
+
+#include<iostream>
+#include<cstdlib>
+
+using namespace std;
+
+int main()
+{
+
+    void srand();
+
+    int k;
+
+    for (k = 0; k <20; k++)
+    {
+        cout << rand() << "\t";
+    }
+    cout << endl;
+}
