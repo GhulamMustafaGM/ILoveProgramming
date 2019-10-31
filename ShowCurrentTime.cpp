47ff2d1ca015b758d0d0c2e986c0fcaf112be1da
Ghulam Mustafa <mustafaji@gmail.com>
2019-10-31 18:31:37 +0100

    DisplayCurrentDate in C++

--- ShowCurrentTime.cpp
+++ ShowCurrentTime.cpp
@@ -8,6 +8,7 @@ using namespace std;
 int main()
 {
 	time_t current = time(0);
+    
 	struct tm *ltm = localtime(&current);
 	cout<<"Current system date is : "<<ltm->tm_mday<<"/"
 	<<1 + ltm->tm_mon<<"/"<<1900 + ltm->tm_year;
