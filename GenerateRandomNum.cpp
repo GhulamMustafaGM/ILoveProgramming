5317dbb088c885282838e4211eb08ef1fe31eca1
Ghulam Mustafa <mustafaji@gmail.com>
2019-10-31 17:06:30 +0100

    GenerateRandomNumbers

---
+++ GenerateRandomNum.cpp
@@ -0,0 +1,21 @@
+// Generate Random numbers
+
+// Generate Random numbers
+
+#include<iostream>
+#include<cstdlib>
+
+using namespace std;
+
+int main()
+{
+
+
+    int k;
+
+    for (k = 0; k <50; k++)
+    {
+        cout << rand() << "\t";
+    }
+    cout << endl;
+}
