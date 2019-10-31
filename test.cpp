355a429c0d75c94f1314e0ef7503952a617a39ac
Ghulam Mustafa <mustafaji@gmail.com>
2019-10-31 17:31:59 +0100

    ExampleTxt

--- test.cpp
+++ test.cpp
@@ -1,6 +1,7 @@
-// Read file
+// Write Content File
 
 #include<iostream>
+#include<cstring>
 #include<cstdlib>
 #include<cstdio>
 #include<fstream>
@@ -9,23 +10,28 @@ using namespace std;
 
 int main()
 {
-    char c[15000];
-    ifstream ifile;
 
-    ifile.open("filename.txt");
-    if(!ifile)
+    ofstream fp;
+
+    char str[150], fName[25];
+
+    cout << "Enter a fileName with extension like file.txt to create file";
+    gets(fName);
+
+    fp.open(fName);
+    if(!fp)
     {
         cout << "Error in opening file...";
         exit(1);
-    }
-    cout << "Data in file = ";
 
-    while(ifile.eof() == 0)
-    {
-        ifile >> c;
-        cout << c << " ";
     }
-    ifile.close();
+    cout<<"Enter few lines of text :\n";
+	while(strlen(gets(str))>0)
+	{
+		fp<<str;
+		fp<<"\n";
+	}
+	fp.close();
 
     cout << endl;
 }
