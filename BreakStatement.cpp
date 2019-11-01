3eccfbd8b9343b4763ad729415abc45ad8d19a7e
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-01 22:37:02 +0100

    BreakStatementInnerLoop

--- BreakStatement.cpp
+++ BreakStatement.cpp
@@ -1,18 +1,21 @@
-// Break program
+// Break statement inner loop program
 
 #include <iostream>
 using namespace std;
 
 int main ()
 {
-    for (int a = 0; a < 10; a++)
-    {
-        if (a == 5)
-        {
+   for (int b = 1; b <= 3; b++)
+   {
+       for (int c = 1; c <= 3; c++)
+       {
+           if (b==2 && c==2)
+           {
             break;
-        }
-         cout << a << endl;
-    }
+           }
+           cout << b << " " << c << endl;
+       }
 
+   }
 
 }
