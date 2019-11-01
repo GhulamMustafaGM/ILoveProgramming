20fa704ece9dcb88eb496e5aec0cc060f8694124
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-01 22:09:14 +0100

    WhileLoop

---
+++ WhileLoop.cpp
@@ -0,0 +1,17 @@
+// While loop program
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+     int number = 1;
+
+     while ( number <= 10)
+     {
+         cout << number << endl;
+         number++;
+     }     
+
+    cout << endl;
+}
