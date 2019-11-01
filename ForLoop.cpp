17103648b620798e083e3d7bf9701840f846e068
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-01 21:59:12 +0100

    ForLopp Program

---
+++ ForLoop.cpp
@@ -0,0 +1,15 @@
+// For loop program
+
+#include <iostream>
+using namespace std;
+
+int main () {
+       int number;
+
+       for (int number = 1; number <= 10; number++)
+        {
+       cout << number << endl;
+        }
+
+        return 0;
+    }
