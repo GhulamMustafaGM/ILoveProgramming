17ab977c769def6be89322255284470e0e33c1e1
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-01 22:23:22 +0100

    DoWhileLoop

---
+++ DoWhileLoop.cpp
@@ -0,0 +1,16 @@
+// Do-While loop program
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int number = 1;
+
+    do {
+        cout << number << endl;
+        number ++;
+    }
+    while(number <= 10);
+
+}
