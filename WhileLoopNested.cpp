45d7645677ae652e30365c65ed5f5cb31ea7a56c
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-01 22:18:51 +0100

    WhileLoopNested

---
+++ WhileLoopNested.cpp
@@ -0,0 +1,20 @@
+// While loop program
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+     int number = 1;
+     while(number <= 3)
+        {
+        int k = 1;
+        while ( k <= 3 )
+        {
+    cout << number << " " << k << endl;
+        k++;
+        }
+        number++;
+
+        }
+}
