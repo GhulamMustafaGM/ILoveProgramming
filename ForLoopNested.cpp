293efbdac2b24b7780598b567eaf950421645bd2
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-01 22:04:12 +0100

    ForLoppNested

---
+++ ForLoopNested.cpp
@@ -0,0 +1,20 @@
+// For loop program
+
+#include <iostream>
+using namespace std;
+
+int main () {
+ {
+
+       int number, k;
+
+       for (number = 1; number <= 3; number++)
+        {
+       for( k =1; k <=3; k++)
+            {
+           cout << number << k << endl;
+            }
+
+        }
+    }
+}
