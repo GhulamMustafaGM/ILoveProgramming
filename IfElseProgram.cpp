b8756d38a359ef43302a919b574736c211a8891f
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-01 21:38:55 +0100

    IfElse Program

---
+++ IfElseProgram.cpp
@@ -0,0 +1,19 @@
+// If Else program
+
+#include <iostream>  
+using namespace std;  
+
+int main () {  
+    int number;  
+    cout<<"Enter a Number: ";  
+    cin>>number;  
+            if (number % 2 == 0)    
+            {    
+                cout<<"It is even number"<<endl;    
+            }   
+            else  
+            {    
+                cout<<"It is odd number"<<endl;    
+            }  
+   return 0;  
+} 
