ca43b4fd3cdcb2787fa89c5752c14ddee20d48a3
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-01 22:31:39 +0100

    BreakStatement

---
+++ BreakStatement.cpp
@@ -0,0 +1,18 @@
+// Break program
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    for (int a = 0; a < 10; a++)
+    {
+        if (a == 5)
+        {
+            break;
+        }
+         cout << a << endl;
+    }
+
+
+}
