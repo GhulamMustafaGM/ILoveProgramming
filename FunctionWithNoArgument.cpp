d4061e8d91d754457beeb1cd0a5243a7eb122eaf
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-02 19:43:19 +0100

    FunctionWithNoArgument

---
+++ FunctionWithNoArgument.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+void welcome()
+
+{
+    cout << "======================="<< endl;
+    cout << "=== Hey guys! ========="<< endl;
+    cout << "========================";
+    return;
+}
+    int main()
+{
+    welcome(); // funcation calling 
+
+    cout << endl;
+    
+    return 0;
+}
