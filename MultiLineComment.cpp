5f772408db1704bd21c09b7c9d59d8aa89d35902
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-02 17:56:00 +0100

    MultiLineComment

---
+++ MultiLineComment.cpp
@@ -0,0 +1,18 @@
+// SingleLine Comment
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    /*
+        initiate and print variable in C++ language.
+    
+    */
+    int k = 38; 
+
+    cout << k << endl;
+
+    return 0;
+}
