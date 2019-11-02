5f162eaee51f3dca714e9722c36160a45a7a18ba
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-02 18:02:21 +0100

    SingleDimensionalArray

---
+++ SingleDimensionalArray.cpp
@@ -0,0 +1,20 @@
+//  Single Dimensional Array
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    /*
+        initiate and print variable in C++ language.
+    
+    */
+    int ArrayNum[5] = {12, 0, 20, 0, 34}; // Declare ArrayNumbers
+    // Traversing array
+
+    for (int  a = 0; a < 5; a++)
+    {
+        cout << ArrayNum[a] << endl; 
+    }
+}
