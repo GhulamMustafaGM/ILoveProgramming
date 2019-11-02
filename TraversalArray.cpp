a1f4e9a7e70b06606b0f03d5d6b2f41006ef61ec
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-02 18:06:23 +0100

    TraversalArray

---
+++ TraversalArray.cpp
@@ -0,0 +1,17 @@
+//  Single Dimensional Array
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int ArrayNum[5] = {12, 0, 20, 0, 34}; // Declare ArrayNumbers
+    // Traversing array
+
+    for (int a: ArrayNum)
+    {
+        cout << a << endl; 
+    }
+}
