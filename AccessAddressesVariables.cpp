80b71a5204bb536cf40834c8e300c1aaea3c56fa
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-03 18:12:22 +0100

    AddressesVariablesAccess

---
+++ AccessAddressesVariables.cpp
@@ -0,0 +1,17 @@
+// Addresses variables
+#include <iostream>
+
+    using namespace std;
+
+    int main()
+    {
+        int num1 = 21;   // declare and initialize
+        int num2 = 52;   // three variables
+        int num3 = 43;
+
+        cout << &num1 << endl;  // print the addresses
+        cout << &num2 << endl;  // of these variables
+        cout << &num3 << endl;
+
+        return 0;
+    }
