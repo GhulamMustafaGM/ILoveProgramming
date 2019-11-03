ce9fcb554af3d6dd49ce8ec6b57c21d2c2a3fbe5
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-03 18:37:23 +0100

    ClassConstructorsDerivedClass

--- ClassOverloadingFunction.cpp
+++ ClassOverloadingFunction.cpp
@@ -1,54 +1,71 @@
-#include <iostream>
-#include <string>
-#include <cstdlib> // for exit ()
+// Constructors in derived class
 
+#include <iostream>
+#include <cstring> // for strcpy(), strcat()
+#include <string>
 
     using namespace std;
 
-    class StackProgram
+    class CounterProgram
     {
-    protected:  // can’t be private
-    enum { MAX = 3 };    //size of stack array
-    int st[MAX];     //stack: array of integers
-    int top;        //index to top of stack
+    protected:                  // not private
+        unsigned int count;         
     public:
-    StackProgram() //constructor
-    { top = -1; }
-    void push(int var) //put number on stack
-    { st[++top] = var; }
-    int pop()       //take number off stack
-    { return st[top--]; }
+       CounterProgram() : count() // constructor, no args
+        {
+        }
+        CounterProgram(int c) : count(c) // constructor, one arg
+        {
+
+        }
+        unsigned int get_count() const // return count
+        {
+            return count;
+        }
+        CounterProgram operator ++ ()  // incr count (prefix)
+        {
+            return CounterProgram(++count);
+        }
 
     };
 
-    class Stack2 : public StackProgram
+class CountDn : public CounterProgram
+{
+public:
+    CountDn() : CounterProgram()       //constructor, no args
     {
-    public:
-    void push(int var) //put number on stack
-    {
-    if(top >= MAX-1) //error if stack full
-    { cout << "\nError: stack is full"; exit(1); }
-    StackProgram::push(var); //call push() in Stack class
-    }
-    int pop() //take number off stack
-    {
-    if(top < 0) //error if stack empty
-    { cout << "\nError: stack is empty\n"; exit(1); }
-    return StackProgram::pop(); //call pop() in Stack class
-    }
-    };
 
-    int main()
+    }
+    CountDn(int c) : CounterProgram(c)     // constructor, 1 arg
     {
-    Stack2 s1;
-    s1.push(11); //push some values onto stack
-    s1.push(22);
-    s1.push(33);
 
-    cout << endl << s1.pop(); //pop some values from stack
-    cout << endl << s1.pop();
-    cout << endl << s1.pop();
-    cout << endl << s1.pop(); //oops, popped one too many...
+    }
+    CountDn operator --()    // decrement count (prefix)
+    {
+        return CountDn(--count);
+    }
+};
+
+int main()
+{
+    CountDn c1;
+    CountDn c2(100);    // class CountDn
+
+    cout << "\nc1=" <<c1.get_count();   
+    cout << "\nc2=" <<c2.get_count();   //display
+
+    ++c1; ++c1; ++c1;           // increment c1
+    cout << "\nc1=" << c1.get_count(); // display it
+
+    --c2; --c2;         //create c3 from c2
+    cout << "\nc2=" << c2.get_count();   // display it
+
+    CountDn c3 = --c2;  // create c3 from c2
+    cout << "\nc3=" << c3.get_count(); // display c3
+
     cout << endl;
+
     return 0;
-    }
+
+}
+
