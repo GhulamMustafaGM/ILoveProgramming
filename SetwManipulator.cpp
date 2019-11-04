32f5041f1c29204191346292f3d5e58066beda9f
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-04 18:19:04 +0100

    SetwManipulator

---
+++ SetwManipulator.cpp
@@ -0,0 +1,18 @@
+// Inputs and outputs
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+    long pop1 = 2425785, pop2 = 47, pop3 = 9761;
+
+    cout << "LOCATION " << "POP. " << endl
+         << "Portcity " << "pop1 " << endl
+         << "Hightown " << "pop2 " << endl
+         << "Lowville " << "pop3 " <<endl;
+
+    return 0;
+}
