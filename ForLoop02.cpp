16610bde8ee4354d67013464ecc430b00d83b232
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-04 18:34:04 +0100

    ForLoop

---
+++ ForLoop02.cpp
@@ -0,0 +1,20 @@
+// For Loop 
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number;
+
+        int a;
+
+        for (a = 0; a < 10; a++)
+        {
+            cout << a * a << " ";
+        }
+        cout << endl;
+
+    return 0;
+}
