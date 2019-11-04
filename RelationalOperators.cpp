2f1eb8aac6b556e182d0c6d71ab47ff3e1daac61
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-04 18:29:58 +0100

    RelationalOperators

--- RelationalOperators.cpp
+++ RelationalOperators.cpp
@@ -1,18 +1,19 @@
 // RelationalOperators
 
 #include <iostream>
+
 using namespace std;
 
 int main()
 {
-    int numb;
+    int number;
 
             cout << "Enter a number: ";
-            cin >> numb;
+            cin >> number;
 
-            cout << "numb < 10 is " << (numb < 10) << endl;
-            cout << "numb > 10 is " << (numb > 10 ) << endl;
-            cout << "numb == 10 is " << (numb == 10 ) << endl;
+            cout << "number < 10 is " << (number < 10) << endl;
+            cout << "number > 10 is " << (number > 10 ) << endl;
+            cout << "number == 10 is " << (number == 10 ) << endl;
 
     return 0;
 }
