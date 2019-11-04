a6e8dc724ea5a808ef7bb7573ec762c09510b1ea
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-04 18:28:23 +0100

    RemainderOperator

--- RemainderOperator.cpp
+++ RemainderOperator.cpp
@@ -1,14 +1,12 @@
-// Inputs and outputs
+// RemainderOperator
 
 #include <iostream>
-#include <iomanip>
+
 using namespace std;
 
 int main()
 
 {
-    long pop1 = 2425733, pop2 = 247, pop3 = 9461, pop4 = 9461;;
-
             cout << 6 % 8 << endl;   
             cout << 7 % 8 << endl;   
             cout << 8 % 8 << endl;   
