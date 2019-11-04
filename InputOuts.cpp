2c56b39576b877f0c2149b99fa0ea18455540c82
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-04 18:16:18 +0100

    InputsOutputsValues

---
+++ InputOuts.cpp
@@ -0,0 +1,21 @@
+// Inputs and outputs
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+    int a;
+
+    cout << "Enter number: ";
+    cin >> a;
+
+    cout << "The number you entered is : ";
+    cout << " and its double is: " << a * 2;
+    cout << endl;
+
+    return 0;
+
+}
