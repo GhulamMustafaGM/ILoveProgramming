6bf47b0d53b2f175cc3a1ddd03843229fa34d572
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-05 03:39:23 +0100

    FindingVariablesSize

---
+++ FindingSizesVariables.cpp
@@ -0,0 +1,18 @@
+// Finding size of variables
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+        cout << "int size = " << sizeof(int) << ",";
+        cout << "short size = " << sizeof(short) << ",";
+        cout << "long size = " << sizeof(long) << ",";
+        cout << "char size = " << sizeof(char) << ", ";
+        cout << "float size = " << sizeof(float) << ", ";
+        cout << "double size = " << sizeof(double) << ",";
+        cout << "bool size = " << sizeof(bool) << ",";
+
+	return 0;
+}
