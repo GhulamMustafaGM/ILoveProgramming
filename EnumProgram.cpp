25779b4f160a00eed57b0be4a9eda049474ce26f
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-05 03:43:39 +0100

    EnumProgram

---
+++ EnumProgram.cpp
@@ -0,0 +1,24 @@
+// Enum Program
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	// Initiate Months as having 12 possible valaues
+	enum months {Jan, Feb, Mar, Apr, May, Jun, Mul, Aug, Sep, Oct, Nov, Dec};
+
+	// Start best month as a variable type months 
+
+	months bestmonth;
+
+	// assign bestm
+
+	bestmonth = Jan;
+
+	if (bestmonth == Jan) 
+	{
+		cout << "I'm not so sure January is the best month\n";
+	}
+
+}
