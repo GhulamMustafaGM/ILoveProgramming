e1c1f740a8f7a6af736f1ffb89fc28ca4b1a2646
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-23 03:22:57 +0100

    StringsProgram

---
+++ StringsProgram01.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string Name;
+            cout << "Enter name: ";
+            getline(cin, name);
+
+            cout << "You are:" << name;
+            
+
+    system("pause");
+    return 0;
+}
