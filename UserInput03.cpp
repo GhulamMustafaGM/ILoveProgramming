c8af2740de9e34e9b015403b42637e23e5b5ede6
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-23 02:55:04 +0100

    UserInput03

---
+++ UserInput03.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number;
+
+    cout << "Enter a number: ";
+    cin >> number;
+
+    cout << "You entered: " << number;
+    
+    system("pause");
+    return 0;
+}
