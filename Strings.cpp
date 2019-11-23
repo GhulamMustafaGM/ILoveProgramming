644a4107af6a19ccf3fe2fae4b6a767ac1beedfe
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-23 03:00:25 +0100

    Strings

---
+++ Strings.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    
+    string greeting = "Hello";
+
+    cout << greeting;
+    cout << endl;
+    
+    system("pause");
+    return 0;
+}
