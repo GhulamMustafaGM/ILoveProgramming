0474fc671fb62604c36fcbfbe02fbd924367cbad
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-23 03:09:37 +0100

    StringsProgram

---
+++ StringsProgram.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    
+    string FirstName = "Peter";
+    string LastName = "Rock";
+
+    string FullName = FirstName + LastName;
+
+    cout << FullName << endl;
+    
+    system("pause");
+    return 0;
+}
