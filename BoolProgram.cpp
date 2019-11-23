6766f71956f95f6fbe2bbb7998f4d6a057dfebd0
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-23 03:38:10 +0100

    BoolProgram

---
+++ BoolProgram.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+// #include<cmath>
+using namespace std;
+
+int main()
+{
+    bool StartCoding = true;
+    bool EndCoding = false;
+
+    cout << StartCoding << endl;
+
+    cout << EndCoding << endl;
+
+    return 0;
+}
