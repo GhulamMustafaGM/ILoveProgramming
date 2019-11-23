ee194eba69febe469752a2534438516321fbc1d6
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-23 03:17:20 +0100

    FindingStringCharacter

---
+++ FindingString.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string StringType = "Hi";
+
+    cout << StringType[1];
+    
+    system("pause");
+    return 0;
+}
