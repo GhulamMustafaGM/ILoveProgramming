e4680087aa0f636fdd6824681c7a8c6ec65f100d
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-23 03:56:22 +0100

    IfElseProgram'

---
+++ IfElseProgram04.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+// #include<cmath>
+using namespace std;
+
+int main()
+{
+    int time = 22;
+
+        string result = (time < 19 ) ? "Good day!" : "Good evening!";
+        cout << result; 
+        cout << endl;
+    
+    return 0;
+}
