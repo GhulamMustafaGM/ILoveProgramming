7e0aa678519617833795eb7b6a5b10e53b95974b
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-23 03:13:47 +0100

    StringsLength

---
+++ StringsLength.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    
+    string txt = "abcdefghijklmnopqrstuvwxyz";
+
+    cout << "The length of the text string is : " << txt.length();
+    cout << "\n";
+    
+    system("pause");
+    return 0;
+}
